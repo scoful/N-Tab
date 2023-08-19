fc0b53b489e8b8eed9db938719e4a45e31e08f74
scoful <1269717999@qq.com>
2023-08-20 00:55:02 +0800

    精简功能，去除广州图书馆wifi登录魔改功能

--- js/gzLibraryInject.js
+++
@@ -1,9 +0,0 @@
-//直接通过方法名覆盖本来的js，增加发消息的功能
-function loginSuccess() {
-    changeElement($("#msg"), "blue", "登录成功，跳转中......");
-    setTimeout(function () {
-        window.location.href = "/Radius/reader/success?type=b&rdid=" + rdid + "&rdPsd=" + rdPsd + "";
-    }, 500);
-    window.postMessage({ action: 'custom-minute', minute: 120 }, '*');
-}
-
